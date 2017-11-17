17762176286fa7ad48f5714b9d0f927a50ddd3d2
Flavius Burca <flaviusburca@users.noreply.github.com>
2017-11-17 12:04:49 +0200

    Added Facebook API

--- setherAPI.sol
+++ setherAPI.sol
@@ -30,7 +30,105 @@ contract SetherI {
     
     function useCoupon(string _coupon);
     function setCustomGasPrice(uint _gasPrice);
-}
+    
+    // Facebook API
+    function sth_fb_getAccessToken(string key, string secret) payable returns (string _accessToken);
+	
+    function sth_fb_getAlbumLikes(string accessToken, string albumId) payable returns (uint32 likes);
+    function sth_fb_addAlbumLike(string accessToken, string albumId) payable returns (bool success);
+    function sth_fb_getAlbumComments(string accessToken, string albumId) payable returns (string[] comments);
+    function sth_fb_addAlbumComment(string accessToken, string albumId, string comment) payable returns (bool success);
+	
+    function sth_fb_getApplicationFeed(string accessToken, string appId) payable returns (string[] feeds);
+    function sth_fb_addToApplicationFeed(string accessToken, string appId, string msg) payable returns (bool success);
+    function sth_fb_getApplicationInsights(string accessToken, string appId) payable returns (string[] insights);
+    function sth_fb_getApplicationPosts(string accessToken, string appId) payable returns (string[] posts);
+    function sth_fb_getApplicationReviews(string accessToken, string appId) payable returns (string[] reviews);
+	
+    function sth_fb_getCheckin(string accessToken, string checkinId) payable returns (string checkin);
+    function sth_fb_getCheckinComments(string accessToken, string checkinId) payable returns (string[] comments);
+	function sth_fb_addCheckinComment(string accessToken, string checkinId, string cmnt) payable returns (bool success);
+	function sth_fb_getCheckinLikes(string accessToken, string checkinId) payable returns (string[] likes);
+	function sth_fb_addCheckinLike(string accessToken, string checkinId) payable returns (bool success);
+	
+	function sth_fb_getComment(string accessToken, string cmntId) payable returns (string comment);
+	function sth_fb_getCommentLikes(string accessToken, string cmntId) payable returns (string[] likes);
+	function sth_fb_addCommentLike(string accessToken, string cmntId) payable returns (bool success);
+	
+	function sth_fb_getEvent(string accessToken, string eventId) payable returns (string evt);
+	function sth_fb_getEventFeed(string accessToken, string eventId) payable returns (string[] feed);
+	function sth_fb_addToEventFeed(string accessToken, string eventId, string msg) payable returns (bool success);
+	function sth_fb_getEventUsers(string accessToken, string eventId, string resposeType) payable returns (string[] users);
+	function sth_fb_rsvpEvent(string accessToken, string eventId, string resposeType) payable returns (bool success);
+	
+	function sth_fb_getFriendList(string accessToken, string listId) payable returns (string details);
+	function sth_fb_getFriendListMembers(string accessToken, string listId) payable returns (string[] members);
+	
+	function sth_fb_getGroup(string accessToken, string groupId) payable returns (string group);
+	function sth_fb_getGroupFeed(string accessToken, string groupId) payable returns (string[] feed);
+	function sth_fb_addToGroupFeed(string accessToken, string groupId, string msg) payable returns (bool success);
+	function sth_fb_getGroupMembers(string accessToken, string groupId) payable returns (string[] members);
+	
+	function sth_fb_getLink(string accessToken, string linkId) payable returns (string lnk);
+	function sth_fb_getLinkComments(string accessToken, string linkId) payable returns (string[] comments);
+	function sth_fb_addLinkComment(string accessToken, string linkId, string cmnt) payable returns (bool success);
+	function sth_fb_getLinkLikes(string accessToken, string linkId) payable returns (string[] likes);
+	function sth_fb_addLinkLike(string accessToken, string linkId) payable returns (bool success);
+	
+	function sth_fb_getPage(string accessToken, string pageId) payable returns (string page);
+	function sth_fb_getPageFeed(string accessToken, string pageId) payable returns (string[] feed);
+	function sth_fb_addToPageFeed(string accessToken, string pageId, string msg) payable returns (bool success);
+	function sth_fb_getPageLinks(string accessToken, string pageId) payable returns (string[] links);
+	function sth_fb_addPageLink(string accessToken, string pageId, string lnk) payable returns (bool success);
+	function sth_fb_getPagePosts(string accessToken, string pageId) payable returns (string[] posts);
+	function sth_fb_getPageEvents(string accessToken, string pageId) payable returns (string[] events);
+	function sth_fb_addPageEvent(string accessToken, string pageId, string eventData) payable returns (bool success);
+	function sth_fb_getPageCheckins(string accessToken, string pageId) payable returns (string[] checkins);
+	
+	function sth_fb_getPhoto(string accessToken, string photoId) payable returns (string photo);
+	function sth_fb_getPhotoComments(string accessToken, string photoId) payable returns (string[] comments);
+	function sth_fb_addPhotoComment(string accessToken, string photoId, string cmnt) payable returns (bool success);
+	function sth_fb_getPhotoLikes(string accessToken, string photoId) payable returns (string[] likes);
+	function sth_fb_addPhotoLike(string accessToken, string photoId) payable returns (bool success);
+	
+	function sth_fb_getPost(string accessToken, string postId) payable returns (string post);
+	function sth_fb_getPostComments(string accessToken, string postId) payable returns (string[] comments);
+	function sth_fb_addPostComment(string accessToken, string postId, string cmnt) payable returns (bool success);
+	function sth_fb_getPostLikes(string accessToken, string postId) payable returns (string[] likes);
+	function sth_fb_addPostLike(string accessToken, string postId) payable returns (bool success);
+	
+	function sth_fb_getStatus(string accessToken, string statusId) payable returns (string statusData);
+	function sth_fb_getStatusComments(string accessToken, string statusId) payable returns (string[] comments);
+	function sth_fb_addStatusComment(string accessToken, string statusId, string cmnt) payable returns (bool success);
+	function sth_fb_getStatusLikes(string accessToken, string statusId) payable returns (string[] likes);
+	function sth_fb_addStatusLike(string accessToken, string statusId) payable returns (bool success);
+	
+	function sth_fb_getUser(string accessToken, string userId) payable returns (string userData);
+	function sth_fb_getUserCheckins(string accessToken, string userId) payable returns (string[] checkins);
+	function sth_fb_addUserCheckin(string accessToken, string userId, string checkin) payable returns (bool success);
+	function sth_fb_getUserEvents(string accessToken, string userId) payable returns (string[] events);
+	function sth_fb_addUserEvent(string accessToken, string userId, string eventData) payable returns (bool success);
+	function sth_fb_getUserFeed(string accessToken, string userId) payable returns (string[] feed);
+	function sth_fb_addToUserFeed(string accessToken, string userId, string msg) payable returns (bool success);
+	function sth_fb_getUserFriends(string accessToken, string userId) payable returns (string[] friends);
+	function sth_fb_hasUserFriend(string accessToken, string friendId) payable returns (bool friend);
+	function sth_fb_getUserGroups(string accessToken, string userId) payable returns (string[] groups);
+	function sth_fb_getUserInterests(string accessToken, string userId) payable returns (string[] interests);
+	function sth_fb_getUserLikes(string accessToken, string userId) payable returns (string[] likes);
+	function sth_fb_hasUserLikedPage(string accessToken, string userId, string pageId) payable returns (bool hasLiked);
+	function sth_fb_getUserLinks(string accessToken, string userId) payable returns (string[] links);
+	function sth_fb_addUserLink(string accessToken, string userId, string linkData) payable returns (bool success);
+	function sth_fb_getUserPosts(string accessToken, string userId) payable returns (string[] posts);
+	function sth_fb_addUserPost(string accessToken, string userId, string post) payable returns (bool success);
+	function sth_fb_getUserStatuses(string accessToken, string userId) payable returns (string[] statuses);
+	function sth_fb_addUserStatus(string accessToken, string userId, string statusMsg) payable returns (bool success);
+	
+	function sth_fb_getVideo(string accessToken, string videoId) payable returns (string videoData);
+	function sth_fb_getVideoComments(string accessToken, string videoId) payable returns (string[] comments);
+	function sth_fb_addVideoComment(string accessToken, string videoId, string cmnt) payable returns (bool success);
+	function sth_fb_getVideoLikes(string accessToken, string videoId) payable returns (string[] likes);
+	function sth_fb_addVideoLike(string accessToken, string videoId) payable returns (bool success);
+} 
 
 contract SetherResolverI {
     function getAddress() returns (address _addr);
@@ -45,6 +143,7 @@ contract usingSether {
     
     SetherResolverI setherResolver;
     SetherI sether;
+    string sether_network_name;
     
     modifier setherAPI {
         if((address(setherResolver)==0)||(getCodeSize(address(setherResolver))==0))
@@ -57,8 +156,8 @@ contract usingSether {
     }
     
      modifier coupon(string code){
-        oraclize = SetherI(setherResolver.getAddress());
-        oraclize.useCoupon(code);
+        sether = SetherI(setherResolver.getAddress());
+        sether.useCoupon(code);
         _;
     }
     
@@ -109,5 +208,13 @@ contract usingSether {
         assembly {
             _size := extcodesize(_addr)
         }
+    }
+    
+    function sether_setNetworkName(string _network_name) internal {
+        sether_network_name = _network_name;
+    }
+    
+    function sether_getNetworkName() internal returns (string) {
+        return sether_network_name;
     }
 }
